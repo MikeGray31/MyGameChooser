13fa1f878187807ba5701836caf4d45be86d7a9c
MikeGray31 <michaelkgray31@gmail.com>
2019-12-23 11:33:33 -0500

    First Commit

---
+++ src/test/java/com/example/MyGameChooser/MyGameChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyGameChooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameChooserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
