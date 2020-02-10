49428d992198949fe5654505d43f1e7487aafec8
MikeGray31 <michaelkgray31@gmail.com>
2020-02-10 14:22:01 -0500

    adding methods to VideoGameServices

--- src/main/java/com/example/MyGameChooser/services/VideoGameServices.java
+++ src/main/java/com/example/MyGameChooser/services/VideoGameServices.java
@@ -1,5 +1,6 @@
 package com.example.MyGameChooser.services;
 
+import com.example.MyGameChooser.models.VideoGame;
 import com.example.MyGameChooser.repositories.VideoGameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,14 @@ public class VideoGameServices {
     @Autowired
     public VideoGameServices(VideoGameRepository videoGameRepository) {
         this.videoGameRepository = videoGameRepository;
+    }
+
+    public VideoGame createGame(VideoGame newGame){
+        this.videoGameRepository.save(newGame);
+        return newGame;
+    }
+
+    public VideoGame findGame(String title){
+        return this.videoGameRepository.findById(title).get();
     }
 }
